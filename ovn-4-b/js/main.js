c4209b20776a68086f91c37feb15bab91d66a980
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-02-04 11:13:40 +0200

    Step 2 ovn4-b fixed: Jag har glömt att använda push först

--- ovn-4-b/js/main.js
+++ ovn-4-b/js/main.js
@@ -1,6 +1,7 @@
 var numbers = [3, 4, 5, 6, 7]
 numbers = numbers.map(x => x*2)
 console.log(numbers)
+numbers.push(100)
 var numbersElement = document.createElement("ul")
 for (number of numbers) {
     let numberElement = document.createElement("li")
