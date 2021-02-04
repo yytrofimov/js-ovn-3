2ebef606ebcd50a83f0115d081a8a37510924982
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-02-04 11:12:15 +0200

    Ovn 4-b up to step 2

--- ovn-4-b/js/main.js
+++ ovn-4-b/js/main.js
@@ -1,3 +1,10 @@
 var numbers = [3, 4, 5, 6, 7]
 numbers = numbers.map(x => x*2)
 console.log(numbers)
+var numbersElement = document.createElement("ul")
+for (number of numbers) {
+    let numberElement = document.createElement("li")
+    numberElement.innerHTML = number
+    numbersElement.appendChild(numberElement)
+}
+document.body.append(numbersElement)
