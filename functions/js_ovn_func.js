010192d988c5f76aee89608fcb5d4953200e9438
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-02-11 13:05:54 +0200

    Uppg2 - done

--- functions/js_ovn_func.js
+++ functions/js_ovn_func.js
@@ -1,6 +1,32 @@
 //Uppgift 1
-function showMeal(mealName)
-{
-    return mealName;
+function showMeal(mealName) {
+  return mealName;
 }
-console.log(showMeal("morgonmål"))
+console.log(showMeal("morgonmål"));
+//Uppgift 2
+var a = 4;
+var b = 7;
+function countResult(a, b, operation) {
+  if (operation == "+") {
+    document.getElementById("result").innerHTML = a + b;
+  } else if (operation == "-") {
+    document.getElementById("result").innerHTML = a - b;
+  } else if (operation == "*") {
+    document.getElementById("result").innerHTML = a * b;
+  } else {
+    document.getElementById("result").innerHTML = a / b;
+  }
+}
+document.getElementById("addition").addEventListener("click", function () {
+  countResult(10, 5, "+");
+});
+document.getElementById("subtraction").addEventListener("click", function () {
+  countResult(10, 5, "-");
+});
+document.getElementById("multiplication").addEventListener("click", function () {
+  countResult(10, 5, "*");
+});
+document.getElementById("division").addEventListener("click", function () {
+  countResult(10, 5, "/");
+});
+
